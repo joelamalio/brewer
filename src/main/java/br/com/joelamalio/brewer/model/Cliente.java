e80a9d2b686fb2ab8510da0028288bf302481a74
Joel Amálio <joel.amalio@gmail.com>
2018-09-06 18:34:39 -0300

    Criação da tela de cadastro de cliente.

---
+++ src/main/java/br/com/joelamalio/brewer/model/Cliente.java
@@ -0,0 +1,18 @@
+package br.com.joelamalio.brewer.model;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Cliente {
+
+	@NotBlank(message = "Nome é obrigatório")
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
