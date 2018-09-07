dea05da65fbd09a1d3bb050d8846600b72a363cb
Joel Amálio <joel.amalio@gmail.com>
2018-09-06 21:16:25 -0300

    Criação do template das entidade estido, cidade e usuário.

---
+++ src/main/java/br/com/joelamalio/brewer/controller/EstiloController.java
@@ -0,0 +1,32 @@
+package br.com.joelamalio.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.joelamalio.brewer.model.Estilo;
+
+@Controller
+public class EstiloController {
+	
+	@RequestMapping("/estilos/novo")
+	public String novo(Estilo estilo) {
+		return "estilo/CadastroEstilo";
+	}
+	
+	@RequestMapping(value = "/estilos/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Estilo estilo, BindingResult result, Model model, RedirectAttributes attributes) {
+		if (result.hasErrors()) {			
+			return novo(estilo);
+		}
+		
+		attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso!");
+		return "redirect:/estilo/novo";
+	}
+	
+}
