494b2fcfb3f5e22b87d3e09c7686ad536e6aec3b
Joel Amálio <joel.amalio@gmail.com>
2018-09-08 11:28:37 -0300

    Modularização do JS.

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,7 +1,21 @@
-$(function() {
-	var decimal = $('.js-decimal');
-	decimal.maskMoney({ decimal: ',', thousands: '.'});
+var Brewer = Brewer || {};
+
+Brewer.MaskMoney = (function() {
+
+	function MaskMoney() {
+		this.decimal = $('.js-decimal');
+		this.plain = $('.js-plain');
+	}
 	
-	var plain = $('.js-plain');
-	plain.maskMoney({ precision : 0, thousands: '.' });
+	MaskMoney.prototype.enable = function() {
+		decimal.maskMoney({ decimal: ',', thousands: '.'});
+		plain.maskMoney({ precision : 0, thousands: '.' });
+	}
+	
+	return MaskMoney;
+}()); // para executar a função é necessário colocar o () no final
+
+$(function() {
+	var maskMoney = new Brewer.MaskMoney();
+	maskMoney.enable();
 });
