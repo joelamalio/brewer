647bc88d72a742b0b7100c19e6ac3c013ffbb790
Joel Amálio <joel.amalio@gmail.com>
2018-09-09 15:20:28 -0300

    Paginação dinâmica.

--- src/main/java/br/com/joelamalio/brewer/repository/helper/cerveja/CervejasImpl.java
+++ src/main/java/br/com/joelamalio/brewer/repository/helper/cerveja/CervejasImpl.java
@@ -1,14 +1,16 @@
 package br.com.joelamalio.brewer.repository.helper.cerveja;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -23,8 +25,27 @@ public class CervejasImpl implements CervejasQueries {
 	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional(readOnly = true)
-	public List<Cerveja> filtrar(CervejaFilter filter) {
+	public Page<Cerveja> filtrar(CervejaFilter filter, Pageable pageable) {
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
+
+		int paginaAtual = pageable.getPageNumber();
+		int totalRegistrosPorPagina = pageable.getPageSize();
+		int primeiroRegistros = paginaAtual * totalRegistrosPorPagina;
+		
+		criteria.setFirstResult(primeiroRegistros);
+		criteria.setMaxResults(totalRegistrosPorPagina);
+		
+		return new PageImpl<Cerveja>(criteria.list(), pageable, total(filter));
+	}
+
+	private Long total(CervejaFilter filter) {
+		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
+		adicionarFiltro(filter, criteria);
+		criteria.setProjection(Projections.rowCount());
+		return (Long) criteria.uniqueResult();
+	}
+	
+	private void adicionarFiltro(CervejaFilter filter, Criteria criteria) {
 		if (filter != null) {
 			if (!StringUtils.isEmpty(filter.getSku())) {
 				criteria.add(Restrictions.eq("sku", filter.getSku()));
@@ -53,12 +74,9 @@ public class CervejasImpl implements CervejasQueries {
 			if (filter.getValorAte() != null) {
 				criteria.add(Restrictions.le("valor", filter.getValorAte()));
 			}
-			
-		}
-		
-		return criteria.list();
+ 		}
 	}
-	
+
 	private boolean isEstiloPresente(CervejaFilter filter) {
 		return filter.getEstilo() != null && filter.getEstilo().getCodigo() != null;
 	}
--- src/main/java/br/com/joelamalio/brewer/repository/helper/cerveja/CervejasQueries.java
+++ src/main/java/br/com/joelamalio/brewer/repository/helper/cerveja/CervejasQueries.java
@@ -1,12 +1,13 @@
 package br.com.joelamalio.brewer.repository.helper.cerveja;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.com.joelamalio.brewer.model.Cerveja;
 import br.com.joelamalio.brewer.repository.filter.CervejaFilter;
 
 public interface CervejasQueries {
 
-	public List<Cerveja> filtrar(CervejaFilter filter);
+	public Page<Cerveja> filtrar(CervejaFilter filter, Pageable pageable);
 	
 }
