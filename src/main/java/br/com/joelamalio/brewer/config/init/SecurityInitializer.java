49ad4b2b662860d06421da412e7203d0453afe52
Joel Amálio <joel.amalio@gmail.com>
2018-09-17 23:25:53 -0300

    Implementação do login com redirecionamento.

---
+++ src/main/java/br/com/joelamalio/brewer/config/init/SecurityInitializer.java
@@ -0,0 +1,9 @@
+package br.com.joelamalio.brewer.config.init;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+	
+	
+}
