fb5f665023f9c9ac8e31a5dedc1db9818664352f
Joel Amálio <joel.amalio@gmail.com>
2018-09-19 18:22:03 -0300

    Configurãção da página 500 de erro.

--- src/main/java/br/com/joelamalio/brewer/controller/CervejasController.java
+++ src/main/java/br/com/joelamalio/brewer/controller/CervejasController.java
@@ -47,7 +47,7 @@ public class CervejasController {
 	
 	@RequestMapping(value = "/novo", method = RequestMethod.POST)
 	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
-		if (result.hasErrors()) {			
+		if (result.hasErrors()) {
 			return novo(cerveja);
 		}
 		
--- src/main/java/br/com/joelamalio/brewer/controller/ErrosController.java
+++ src/main/java/br/com/joelamalio/brewer/controller/ErrosController.java
@@ -2,13 +2,19 @@ package br.com.joelamalio.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrosController {
 
 	@GetMapping("/404")
-	public String paginaNaoEncontrada() {
+	public String pageNotFound() {
 		return "404";
+	}
+	
+	@RequestMapping("/500")
+	public String internalServerError() {
+		return "500";
 	}
 	
 }
